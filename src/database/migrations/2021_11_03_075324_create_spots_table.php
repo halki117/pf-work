0c6fdd683aa7c6c1b83872030be8972bea6cae03
halki <s.halki0203@icloud.com>
2021-11-03 17:05:04 +0900

    Create 2021_11_03_075324_create_spots_table.php
    spotsテーブルの作成

---
+++ src/database/migrations/2021_11_03_075324_create_spots_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address');
+            $table->string('image');
+            $table->text('review');
+            $table->boolean('public');
+            $table->float('longitude');
+            $table->float('latitude');
+            $table->uuid('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+}
