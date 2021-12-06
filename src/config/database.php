f0763bdc4320b50d193a367b77f0db56ce48219a
halki <s.halki0203@icloud.com>
2021-12-06 21:45:11 +0900

    Update database.php

--- src/config/database.php
+++ src/config/database.php
@@ -113,7 +113,7 @@ return [
 
         'circle_testing' => [
             'driver' => 'mysql',
-            'host' => 'db',
+            'host' => 'localhost',
             'port' => '3306',
             'database' => 'test_PortFolio',
             'username' => 'default',
