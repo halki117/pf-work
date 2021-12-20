1ec8048190a6ab04485b9170d2297674386fd9e6
halki <s.halki0203@icloud.com>
2021-12-20 16:31:23 +0900

    Update AppServiceProvider.php

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -25,8 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     { 
-        if(app()->isProduction()){
-            URL::forceScheme('https');
-        }
+        //
     }
 }
