b43c861f90a99c7fa659cedf37492be1ceab6c6e
halki <s.halki0203@icloud.com>
2022-01-11 13:23:21 +0900

    Update index.blade.php

--- src/resources/views/spots/index.blade.php
+++ src/resources/views/spots/index.blade.php
@@ -4,7 +4,7 @@
 <div class="hero">
   
   <picture>
-    <h1 class="hero_top" style="word-break: break-all;">Wellcome to RELIANCE!!!<br>Enjoy Good Discovery!!!</h1>
+    <h1 class="hero_top">Wellcome to RELIANCE!!!<br>Enjoy Good Discovery!!!</h1>
     <div class="fade-img">
       <img src="https://manablog.org/wp-content/uploads/2015/02/f1a9fcfd142669cb8ab064cb90251123.jpg" alt="">
       <img src="https://tanabota.blog/wp-content/uploads/2020/01/interview2001-007.jpg" alt="">
